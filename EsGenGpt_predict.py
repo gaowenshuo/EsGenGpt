09bc0e08a4d3ec2a3def4f1ec3ce821b22dc76c3
gaowenshuo <90968450+gaowenshuo@users.noreply.github.com>
2022-05-05 20:58:48 +0800

    Update EsGenGpt_predict.py

--- EsGenGpt_predict.py
+++ EsGenGpt_predict.py
@@ -8,9 +8,15 @@ if __name__ == '__main__':
     text = open ( 'data.txt' , 'r' , encoding="utf-8" ).read ()
     train_dataset = CharDataset ( text , block_size )
     # 读取模型
-    model = torch.load ( "model.pth" , map_location="cpu" )
-    context = "北京大学是世界一流大学"
-    x = torch.tensor ( [ train_dataset.stoi[ s ] for s in context ] , dtype=torch.long )[ None , ... ]
-    y = sample ( model , x , 2000 , temperature=1.0 , sample=True , top_k=10 )[ 0 ]
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = torch.load ( "model.pth" , map_location=device )
+    context = input("请输入第一句话：")
+    length = input("请输入生成长度：")
+    try:
+        length = int(length)
+    except:
+        length = 200
+    x = torch.tensor ( [ train_dataset.stoi[ s ] for s in context ] , dtype=torch.long )[ None , ... ].to(device)
+    y = sample ( model , x , length , temperature=1.0 , sample=True , top_k=10 )[ 0 ]
     completion = ''.join ( [ train_dataset.itos[ int ( i ) ] for i in y ] )
     print ( completion )
